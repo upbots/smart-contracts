667c04f9460549ea922df83f640b4cefa1e34bab
tki <tki.wroc@gmail.com>
2020-12-09 17:06:51 +0100

    Switch rinkeby to https provider

--- truffle-config.js
+++ truffle-config.js
@@ -74,9 +74,8 @@ module.exports = {
       provider: () =>
         new HDWalletProvider(
           mnemonic,
-          `wss://rinkeby.infura.io/ws/v3/${PROJECT_ID}`
+          `https://rinkeby.infura.io/v3/${PROJECT_ID}`
         ),
-      websockets: true, // (default: false)
       network_id: 4,
       gas: 4500000,
       gasPrice: 10000000000,
